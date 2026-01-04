f3034544cb0f1368ecf048ba19fa69f7f6f92f6a
squach28 <squach3443@gmail.com>
2026-01-04 08:36:10 -0800

    add @Data annotation

--- src/main/java/com/squach/out_of_the_chat/entities/Trip.java
+++ src/main/java/com/squach/out_of_the_chat/entities/Trip.java
@@ -1,12 +1,14 @@
 package com.squach.out_of_the_chat.entities;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
+@Data
 @Table(name="Trips")
 public class Trip {
     @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     @Column(name="id")
     private Integer id;
 
