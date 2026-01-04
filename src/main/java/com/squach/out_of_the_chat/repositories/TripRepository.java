9e2557dee26bde616cdfbb0915c3c45b33cb92ac
squach28 <squach3443@gmail.com>
2026-01-04 08:36:38 -0800

    add TripRepository as JpaRepository

--- src/main/java/com/squach/out_of_the_chat/repositories/TripRepository.java
+++ src/main/java/com/squach/out_of_the_chat/repositories/TripRepository.java
@@ -1,2 +1,9 @@
-package com.squach.out_of_the_chat.repositories;public class TripRepository {
+package com.squach.out_of_the_chat.repositories;
+
+import com.squach.out_of_the_chat.entities.Trip;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TripRepository extends JpaRepository<Trip, Integer> {
 }
