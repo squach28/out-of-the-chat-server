6074a33f4c8dd61d79c552359eca084467dfe31e
squach28 <squach3443@gmail.com>
2026-01-04 09:08:15 -0800

    add TripRepositoryTest with tests for creating and fetching trips

--- src/test/java/com/squach/out_of_the_chat/repositories/TripRepositoryTest.java
+++ src/test/java/com/squach/out_of_the_chat/repositories/TripRepositoryTest.java
@@ -1,2 +1,62 @@
-package com.squach.out_of_the_chat.repositories;public class TripRepositoryTest {
+package com.squach.out_of_the_chat.repositories;
+
+import com.squach.out_of_the_chat.OutOfTheChatApplication;
+import com.squach.out_of_the_chat.entities.Trip;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+@SpringBootTest
+@Transactional
+public class TripRepositoryTest {
+
+    @Autowired
+    private TripRepository tripRepository;
+
+    @BeforeEach
+    public void setup() {
+        tripRepository.deleteAll();
+    }
+
+    @Test
+    public void createTripTest() {
+        String tripName = "Test Name";
+        String tripDescription = "Test Description";
+
+        Trip trip = new Trip();
+        trip.setName(tripName);
+        trip.setDescription(tripDescription);
+
+        Trip savedTrip = tripRepository.save(trip);
+
+        long expectedCount = 1;
+
+        assertThat(savedTrip.getId()).isNotNull();
+        assertThat(savedTrip.getName()).isEqualTo(tripName);
+        assertThat(savedTrip.getDescription()).isEqualTo(tripDescription);
+        assertThat(tripRepository.count()).isEqualTo(expectedCount);
+    }
+
+    @Test
+    public void getTripByIdTest() {
+        String tripName = "Test Name";
+        String tripDescription = "Test Description";
+
+        Trip trip = new Trip();
+        trip.setName(tripName);
+        trip.setDescription(tripDescription);
+
+        Trip savedTrip = tripRepository.save(trip);
+
+        Trip expectedTrip = tripRepository.getReferenceById(savedTrip.getId());
+
+        assertThat(expectedTrip).isNotNull();
+        assertThat(expectedTrip.getName()).isEqualTo(tripName);
+        assertThat(expectedTrip.getDescription()).isEqualTo(tripDescription);
+    }
 }
