28f3b51c47b9609cfc1d59723df70972ce09b6f6
squach28 <squach3443@gmail.com>
2026-01-04 08:37:28 -0800

    add TripService with getTripById and createTrip functions

--- src/main/java/com/squach/out_of_the_chat/services/TripService.java
+++ src/main/java/com/squach/out_of_the_chat/services/TripService.java
@@ -1,2 +1,31 @@
-package com.squach.out_of_the_chat.services;public class TripService {
+package com.squach.out_of_the_chat.services;
+
+import com.squach.out_of_the_chat.entities.Trip;
+import com.squach.out_of_the_chat.repositories.TripRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TripService {
+
+    private final TripRepository tripRepository;
+
+    @Autowired
+    public TripService(TripRepository tripRepository) {
+        this.tripRepository = tripRepository;
+    }
+
+    public Trip getTripById(int id) {
+        return this.tripRepository.getReferenceById(id);
+    }
+
+    public Trip createTrip(String name, String description) {
+        Trip trip = new Trip();
+        trip.setName(name);
+        trip.setDescription(description);
+
+        Trip savedTrip = this.tripRepository.save(trip);
+
+        return savedTrip;
+    }
 }
