6ff89c5c641970a062c0fe1be644a105a3ac4068
squach28 <squach3443@gmail.com>
2026-01-03 23:15:27 -0800

    initial project

---
+++ src/main/java/com/squach/out_of_the_chat/OutOfTheChatApplication.java
@@ -0,0 +1,13 @@
+package com.squach.out_of_the_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutOfTheChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutOfTheChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/squach/out_of_the_chat/OutOfTheChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.squach.out_of_the_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutOfTheChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
