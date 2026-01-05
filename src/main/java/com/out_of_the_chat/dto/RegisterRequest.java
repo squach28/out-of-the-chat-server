af426bb2a36256d64ef3fa0d63f9f83a3109a7c8
squach28 <squach3443@gmail.com>
2026-01-05 00:32:08 -0800

    add dto for RegisterRequest and RegisterResponse

--- src/main/java/com/out_of_the_chat/dto/RegisterRequest.java
+++ src/main/java/com/out_of_the_chat/dto/RegisterRequest.java
@@ -1,2 +1,10 @@
-package com.out_of_the_chat.dto;public class RegisterRequest {
+package com.out_of_the_chat.dto;
+
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+    private String email;
+    private String username;
+    private String password;
 }
--- src/main/java/com/out_of_the_chat/dto/RegisterResponse.java
+++ src/main/java/com/out_of_the_chat/dto/RegisterResponse.java
@@ -1,2 +1,9 @@
-package com.out_of_the_chat.dto;public class RegisterResponse {
+package com.out_of_the_chat.dto;
+
+import lombok.Data;
+
+@Data
+public class RegisterResponse {
+    private String email;
+    private String username;
 }
