2da73b7c0246c2460fe9773aef47e5e3de46ee2c
squach28 <squach3443@gmail.com>
2026-01-05 00:32:31 -0800

    add UserRepository

--- src/main/java/com/out_of_the_chat/repositories/UserRepository.java
+++ src/main/java/com/out_of_the_chat/repositories/UserRepository.java
@@ -1,2 +1,11 @@
-package com.out_of_the_chat.repositories;public class UserRepository {
+package com.out_of_the_chat.repositories;
+
+import com.out_of_the_chat.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    User findByUsername(String username);
+    User findByEmail(String email);
 }
