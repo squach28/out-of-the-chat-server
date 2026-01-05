345103a30fc1cd7b2622de58b5455885bc164341
squach28 <squach3443@gmail.com>
2026-01-05 00:31:37 -0800

    add User entity

--- src/main/java/com/out_of_the_chat/entities/User.java
+++ src/main/java/com/out_of_the_chat/entities/User.java
@@ -1,2 +1,25 @@
-package com.out_of_the_chat.entities;public class User {
+package com.out_of_the_chat.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Entity
+@Data
+@Table(name="users")
+public class User {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String email;
+
+    private String username;
+
+    private String password;
+
 }
