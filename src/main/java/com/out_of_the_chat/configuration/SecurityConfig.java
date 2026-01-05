5dbe07b6622fca5b4c3b77aa2b6f3a690bcf3e47
squach28 <squach3443@gmail.com>
2026-01-05 00:34:20 -0800

    allow all requests for register and login, add PasswordEncoder

--- src/main/java/com/out_of_the_chat/configuration/SecurityConfig.java
+++ src/main/java/com/out_of_the_chat/configuration/SecurityConfig.java
@@ -2,13 +2,33 @@ package com.out_of_the_chat.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+@EnableWebSecurity
 @Configuration
-public class SecurityConfiguration {
+public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) {
-        return http.csrf(csrf -> csrf.disable()).build();
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/register", "/auth/login").permitAll()
+                        .anyRequest().authenticated())
+                .build();
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+//    @Bean
+//    public AuthenticationProvider authenticationProvider() {
+//
+//    }
 }
