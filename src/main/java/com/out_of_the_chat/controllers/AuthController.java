e28069d090d23d3abf7c5334bfd195034f61c52a
squach28 <squach3443@gmail.com>
2026-01-05 00:33:04 -0800

    add AuthController with register endpoint

--- src/main/java/com/out_of_the_chat/controllers/AuthController.java
+++ src/main/java/com/out_of_the_chat/controllers/AuthController.java
@@ -1,7 +1,29 @@
 package com.out_of_the_chat.controllers;
 
+import com.out_of_the_chat.dto.RegisterRequest;
+import com.out_of_the_chat.dto.RegisterResponse;
+import com.out_of_the_chat.services.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class UserController {
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final AuthService authService;
+    @Autowired
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<RegisterResponse> register(@RequestBody RegisterRequest request) {
+        RegisterResponse response = this.authService.register(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
 }
