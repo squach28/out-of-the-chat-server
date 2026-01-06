2703e7db5c28297f0814d3067ba70855e961d2cc
squach28 <squach3443@gmail.com>
2026-01-05 23:30:16 -0800

    add exception handling for getTrip

--- src/main/java/com/out_of_the_chat/controllers/TripController.java
+++ src/main/java/com/out_of_the_chat/controllers/TripController.java
@@ -2,8 +2,10 @@ package com.out_of_the_chat.controllers;
 
 import com.out_of_the_chat.dto.TripRequest;
 import com.out_of_the_chat.entities.Trip;
+import com.out_of_the_chat.exceptions.TripNotFoundException;
 import com.out_of_the_chat.services.TripService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,9 +19,9 @@ public class TripController {
         this.tripService = tripService;
     }
 
-    @GetMapping
-    public Trip getTrip(@PathVariable int tripId) {
-        return this.tripService.getTripById(tripId);
+    @GetMapping("/{id}")
+    public Trip getTrip(@PathVariable int id) {
+        return tripService.getTripById(id);
     }
 
     @PostMapping
@@ -33,6 +35,9 @@ public class TripController {
             System.out.println(e.getMessage());
             return ResponseEntity.internalServerError().build();
         }
-
     }
+
+    @ExceptionHandler(TripNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    void handleNotFound() {}
 }
