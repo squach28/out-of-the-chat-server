0e90f54bd7a99d37b76fc8a187aae700e7cfb077
squach28 <squach3443@gmail.com>
2026-01-05 23:31:31 -0800

    add CustomUserDetailsService to load user by username

--- src/main/java/com/out_of_the_chat/services/CustomUserDetailsService.java
+++ src/main/java/com/out_of_the_chat/services/CustomUserDetailsService.java
@@ -1,16 +1,31 @@
 package com.out_of_the_chat.services;
 
+import com.out_of_the_chat.entities.User;
 import com.out_of_the_chat.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import org.springframework.security.core.userdetails.UserDetailsService
 
 @Service
-public class UserDetailsService implements UserDetailsService {
+public class CustomUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
-    public UserDetailsService(UserRepository userRepository) {
+    @Autowired
+    public CustomUserDetailsService(UserRepository userRepository) {
         this.userRepository = userRepository;
 
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .build();
     }
 }
