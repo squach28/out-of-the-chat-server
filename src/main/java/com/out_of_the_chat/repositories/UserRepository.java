665c4a837a6d04fbd32c06f2ac7ec0d4c55768ae
squach28 <squach3443@gmail.com>
2026-01-05 23:30:59 -0800

    add UserRepository with additional functions of findByUsername and findByEmail

--- src/main/java/com/out_of_the_chat/repositories/UserRepository.java
+++ src/main/java/com/out_of_the_chat/repositories/UserRepository.java
@@ -4,10 +4,11 @@ import com.out_of_the_chat.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
-    User findByUsername(String username);
-    User findByEmail(String email);
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
 }
