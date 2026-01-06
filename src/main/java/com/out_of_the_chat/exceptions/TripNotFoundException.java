f7cd853d74b954b1e6a1ffcd2f838d5bbdc7ea70
squach28 <squach3443@gmail.com>
2026-01-05 23:29:45 -0800

    add TripNotFoundException

--- src/main/java/com/out_of_the_chat/exceptions/TripNotFoundException.java
+++ src/main/java/com/out_of_the_chat/exceptions/TripNotFoundException.java
@@ -1,2 +1,8 @@
-package com.out_of_the_chat.exceptions;public class TripNotFoundException {
+package com.out_of_the_chat.exceptions;
+
+public class TripNotFoundException extends RuntimeException {
+
+    public TripNotFoundException(int id) {
+        super("Trip not found: " + id);
+    }
 }
