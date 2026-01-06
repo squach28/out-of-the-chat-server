62f3e2e42a3ab015f0c2364e1614836d050e34e4
squach28 <squach3443@gmail.com>
2026-01-05 23:32:34 -0800

    add jwtService and authenticationManager, add login endpoint

--- src/main/java/com/out_of_the_chat/controllers/AuthController.java
+++ src/main/java/com/out_of_the_chat/controllers/AuthController.java
@@ -1,29 +1,68 @@
 package com.out_of_the_chat.controllers;
 
+import com.out_of_the_chat.dto.LoginRequest;
 import com.out_of_the_chat.dto.RegisterRequest;
 import com.out_of_the_chat.dto.RegisterResponse;
 import com.out_of_the_chat.services.AuthService;
+import com.out_of_the_chat.services.JwtService;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Duration;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
     private final AuthService authService;
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
+    private final static long TOKEN_DURATION = 1;
+    private final static String SAME_SITE = "Strict";
+
     @Autowired
-    public AuthController(AuthService authService) {
+    public AuthController(AuthService authService, AuthenticationManager authenticationManager, JwtService jwtService) {
         this.authService = authService;
+        this.authenticationManager = authenticationManager;
+        this.jwtService = jwtService;
     }
 
     @PostMapping("/register")
     public ResponseEntity<RegisterResponse> register(@RequestBody RegisterRequest request) {
         RegisterResponse response = this.authService.register(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody LoginRequest request, HttpServletResponse response) {
+        Authentication auth = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getUsername(),
+                request.getPassword()));
+
+        String accessToken = jwtService.generateToken(request.getUsername());
+
+        ResponseCookie cookie = ResponseCookie.from("access_token", accessToken)
+                .httpOnly(true)
+                .path("/")
+                .maxAge(Duration.ofHours(TOKEN_DURATION))
+                .sameSite(SAME_SITE)
+                .build();
+
+        response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
+
+        return ResponseEntity.ok().build();
+
     }
 }
